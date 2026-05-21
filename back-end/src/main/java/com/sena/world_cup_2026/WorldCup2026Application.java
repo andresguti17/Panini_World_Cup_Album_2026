b808c53fc715bbb424389f0b210d071dca3a5c6f
Diego Andrés Gutiérrez Nuñez <diangunu1709@gmail.com>
2026-05-21 16:57:38 -0500

    feat: Estructura inicial del proyecto

---
+++ back-end/src/main/java/com/sena/world_cup_2026/WorldCup2026Application.java
@@ -0,0 +1,13 @@
+package com.sena.world_cup_2026;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorldCup2026Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorldCup2026Application.class, args);
+	}
+
+}
---
+++ back-end/src/test/java/com/sena/world_cup_2026/WorldCup2026ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sena.world_cup_2026;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorldCup2026ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
